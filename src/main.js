ff84dd259628c8d6845e4672ef08b9367329d59e
Olga <vesnushka.ol@mail.ru>
2020-05-04 14:55:29 +0200

    Переключит контроллер на работу с моделью

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import SiteMenuComponent from "./components/site-menu.js";
 import FilterComponent from "./components/filter.js";
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board";
+import TasksModel from "./models/tasks";
 import {generateFilters} from "./mock/filter.js";
 import {generateTasks} from "./mock/task.js";
 import {render} from "./utils/render.js";
@@ -13,6 +14,8 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 const filters = generateFilters(tasks);
 
 render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREND);
@@ -21,6 +24,6 @@ render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREND);
 const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREND);
 
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 
 boardController.render(tasks);
